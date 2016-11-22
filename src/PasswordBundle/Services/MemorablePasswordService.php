b21e9a93c7fb45ac93712e06c8f9546edaaf9c22
kane <kanemenicou@gmail.com>
2016-11-22 20:30:17 +0000

    Added the ability for the user to choose whether they would like there password to be memorable.

--- src/PasswordBundle/Services/MemorablePasswordService.php
+++ src/PasswordBundle/Services/MemorablePasswordService.php
@@ -19,44 +19,53 @@ class MemorablePasswordService
         $length = 4, $allowLetters = true, $allowNumbers = true, $allowSymbols = true, $allowUpperCase = true
     )
     {
-        $allowedChars = [];
         $numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-        $words =[ 'apple', 'brown', 'cat', 'duff', 'egghead', 'for', 'girl', 'hip', 'internet', 'jumping', 'kicking',
-            'lips', 'motherboard', 'never', 'open', 'pee', 'quickly', 'resolve', 'status', 'technology', 'uniforms',
-            'vegan', 'what', 'xylophone', 'yoop' ,'zeep'
-        ];
         $symbols = ['{', '}', '(', ')', '[', "]", '#', ':', ';', '^', ',', '.', '?', '!', '|', '&', '_', '`','~', '@',
             '$', '%', '/', '\\', '=', '+', '*', '-','"', "''"
         ];
-        $upperWords = ['Alphabet','Beet','Control','Drizzle','Exuvium','Fall','Gweduck','Hommock','Injured','Jumpoff',
-            'Knobbly','Lunch','Money','Nymphal','Offence','Quippus','Reequip','Sitting','Toenail','Uniform', 'Vowels',
-            'Wavy','Xylophone','Yakkity', 'Zulue'
+        $constants =[ 'b','b', 'c','c','c', 'd','d','d','d','d', 'f','f','f', 'g','g','g', 'h','h','h','h','h','h', 'j',
+            'k', 'l', 'l','l','l','l', 'm','m','m', 'n','n','n','n','n','n', 'p', 'p', 'q', 'r','r','r','r','r', 's',
+            's','s','s','s','s','s', 't','t','t','t','t','t','t','t','t', 'v', 'w','w','w', 'x', 'y', 'y' ,'z'
         ];
-        $constants =[ 'b', 'c', 'd',  'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n',  'p', 'p', 'q', 'r', 's', 't', 'v', 'w',
-            'x', 'y' ,'z'
-        ];
-        $vowels = ['a', 'e', 'i', 'o', 'u'];
+        $vowels = ['a','a','a','a','a','a','a','a', 'e','e','e','e','e','e','e','e','e','e','e','e', 'i','i','i','i',
+            'i','i', 'o','o','o','o','o','o','o','o', 'u','u','u'];
+
         $word = [];
         $constantNumber = 0;
         $passwordLength = 0;
 
         while ($length > $passwordLength) {
-            $index = rand(1,4);
-            if ($index === 1 || $constantNumber -1 === $length){
-                array_push($word, $vowels[rand(0,4)]);
-                $passwordLength += 1;
-            }else{
-                $letter = $constants[rand(0,20)];
-                if ($letter === 'q' && $passwordLength + 1 !== $length ){
-                    array_push($word, $letter, 'u');
-                }elseif ($letter !== 'q'){
+            if ($allowLetters === true) {
+                $index = rand(1, 4);
+                if ($index === 1 || $constantNumber - 1 === $length) {
+                    array_push($word, $vowels[rand(0, 36)]);
+                    $passwordLength += 1;
+                } else {
+                    $letter = $constants[rand(0, 69)];
                     array_push($word, $letter);
+                    $passwordLength += 1;
+                    $constantNumber += 1;
                 }
-                $constantNumber += 1;
-                $passwordLength += 1;
+                if ($word[$passwordLength -1] === 'q' && $length > $passwordLength){
+                    array_push($word, 'u');
+                    $passwordLength += 1;
+            }
+            }
+            if ($length === $passwordLength) {
+                if (
+                    $word[$length - 1] === 'i' ||
+                    $word[$length - 1] === 'u' ||
+                    $word[$length - 1] === 'v' ||
+                    $word[$length - 1] === 'j' ||
+                    $word[$length - 1] === 'q' ||
+                    $word[$length - 1] === 'v' ||
+                    $word[$length - 1] === 'u'
+                ) {
+                    $passwordLength = 0;
+                    $word = [];
+                }
             }
         }
-
         return implode('', $word);
     }
 }
